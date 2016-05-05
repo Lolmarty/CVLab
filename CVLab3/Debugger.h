3c752a2d0f2aca7e90999726ded0be302568f7b8
Lolmarty <lysenko.andriy@gmail.com>
2016-05-05 19:14:30 +0300

    i iz stoopid

---
+++ CVLab3/Debugger.h
@@ -0,0 +1,26 @@
+#include <opencv2/core/core.hpp>
+#include "Settings.h"
+
+namespace Tracking
+{
+	class Debugger
+	{
+	public:
+		static Debugger& Instance()
+		{
+			static Debugger singletonDebuggerInstance;
+			return singletonDebuggerInstance;
+		}
+		void AddImage(std::string label, cv::Mat image);
+		void AddVariable(std::string label, int variable);
+		void AddVariable(std::string label, double variable);
+		void AddVariable(std::string label, bool variable);
+		void Show();
+	private:
+		std::map<std::string, cv::Mat> debugImages;
+		std::map<std::string, std::string> debugVariables;
+		int maxImagesWidth;
+		Debugger(){ maxImagesWidth = Settings::Instance().IntGet("MAX_IMAGES_WIDTH"); }
+	};
+
+}
